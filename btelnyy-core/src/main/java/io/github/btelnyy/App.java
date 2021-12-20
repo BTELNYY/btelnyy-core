15918cbd06932508746d91be8b9d7d9d742ae192
BTELNYY <74313030+BTELNYY@users.noreply.github.com>
2021-12-20 15:30:33 -0600

    code later, tried.

---
+++ btelnyy-core/src/main/java/io/github/btelnyy/App.java
@@ -0,0 +1,12 @@
+package io.github.btelnyy.btelnyy;
+import org.bukkit.plugin.java.JavaPlugin;
+public class App extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello, SpigotMC!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("See you again, SpigotMC!");
+    }
+}
