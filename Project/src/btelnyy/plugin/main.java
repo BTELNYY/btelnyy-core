0bb58185227dc2ad3b893d005e528b4ef343b1ec
BTELNYY <74313030+BTELNYY@users.noreply.github.com>
2022-01-29 08:24:07 -0600

    yay, plugin works. need to code now.

---
+++ Project/src/btelnyy/plugin/main.java
@@ -0,0 +1,16 @@
+package btelnyy.plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+public class main extends JavaPlugin {
+		// TODO Auto-generated method stub
+	    // Fired when plugin is first enabled
+		
+	    @Override
+	    public void onEnable() {}
+	    
+	    
+	    // Fired when plugin is disabled
+	    @Override
+	    public void onDisable() {}
+	    
+}
+
