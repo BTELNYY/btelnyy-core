52e60b375de8c5cf82d41d92c6cefc686f674292
writingbettercodethanyou <writingbettercodethanyou@gmail.com>
2022-04-24 20:30:51 -0600

    Optimized CommandMySpawn to use chad logic

--- src/main/java/me/btelnyy/core/command/CommandMySpawn.java
+++ src/main/java/me/btelnyy/core/command/CommandMySpawn.java
@@ -3,31 +3,24 @@ package me.btelnyy.core.command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.entity.Player;
 import org.bukkit.command.*;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 
 public class CommandMySpawn implements CommandExecutor {
-    public boolean onCommand(CommandSender sender, Command cmd, String arg, String[] args){
-        if(!(sender instanceof Player)){
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+        if(!(sender instanceof Player player)){
             sender.sendMessage(ChatColor.RED + "Error: You must be a player in order to run this command.");
             return true;
         }
-        Player player = (Player) sender;
-        Location bedLocation = player.getBedSpawnLocation();
-        Location spawn = Bukkit.getServer().getWorld(player.getLocation().getWorld().getName()).getSpawnLocation();
-        int spawn_x = spawn.getBlockX();
-        int spawn_y = spawn.getBlockY();
-        int spawn_z = spawn.getBlockZ();
-        if(bedLocation == null){
+
+        Location spawnLocation = player.getBedSpawnLocation();
+        if(spawnLocation == null) {
+            spawnLocation = player.getWorld().getSpawnLocation();
             sender.sendMessage(ChatColor.GRAY + "You do not have a bed.");
-            sender.sendMessage(ChatColor.GRAY + "Your respawn location: " + spawn_x + " " + spawn_y + " " + spawn_z);
-            return true;
         }
-        int bed_x = bedLocation.getBlockX();
-        int bed_y = bedLocation.getBlockY();
-        int bed_z = bedLocation.getBlockZ();
-        sender.sendMessage(ChatColor.GRAY + "Your respawn location: " + bed_x + " " + bed_y + " " + bed_z);
+
+        sender.sendMessage(String.format("%sYour respawn location: %d %d %d", ChatColor.GRAY, spawnLocation.getBlockX(), spawnLocation.getBlockY(), spawnLocation.getBlockZ()));
         return true;
     }
 }
